6bd1bcfa3da924b1fcebd7042c2789064286f5ac
scifuentes78 <96213562+scifuentes78@users.noreply.github.com>
2021-12-18 01:20:35 -0300

    Update sklearn_transformers.py

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,4 +1,8 @@
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.preprocessing import LabelEncoder
+
+
+X[['koi_pdisposition']] = pd.DataFrame(label_encoder.transform(X[['koi_pdisposition']]).transpose())
 
 
 # All sklearn Transforms must have the `transform` and `fit` methods
@@ -14,3 +18,20 @@ class DropColumns(BaseEstimator, TransformerMixin):
         data = X.copy()
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+
+class Label_col(BaseEstimator, TransformerMixin):
+    def __init__(self, columns):
+        self.columns = columns
+
+    def fit(self, X, y=None):
+        label_encoder=LabelEncoder()
+        label_encoder.fit(X[[self.columns]])
+        return self
+
+    def transform(self, X):
+        # Primeiro realizamos a cópia do dataframe 'X' de entrada
+        data = X.copy()
+        data[self.columns] = label_encoder.transform(data[self.columns])
+        # Retornamos um novo dataframe sem as colunas indesejadas
+        return data
+    
